da1b01f65743b55aef9479651872a6b414e6de40
sjohnstone18 <sjohnstone18@my.whitworth.edu>
2017-05-15 13:20:18 -0700

    started project
    made two classes

---
+++ Final project/Medical.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef Medical_h
+#define Medical_h
+#include <iostream>
+#include "Person.h"
+#include <queue>
+using namespace std;
+
+
+
+class Medical {
+private:
+	int maxSeverity;
+	queue<Person*> helper;
+
+
+
+
+
+
+
+
+
+
+};
+
+
+#endif // !Medical.h
---
+++ Final project/Person.h
@@ -0,0 +1,27 @@
+#ifndef Person_h
+#define Person_h
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Person{
+private:
+	string name;
+	int severity;
+	vector<int> history;
+
+
+
+
+public:
+
+
+
+
+};
+
+
+
+#endif // !Person.h
